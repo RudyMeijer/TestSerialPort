7bef80166b447d4c75c8160fecaa6e851df465db
MeijerR <MeijerR@NLHLP626>
2018-05-04 14:09:35 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TestSerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtTransmit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(81, 3);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(87, 24);
+            this.cmbPort.TabIndex = 1;
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(0, 48);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.Size = new System.Drawing.Size(380, 138);
+            this.txtReceive.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(188, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 28);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtTransmit
+            // 
+            this.txtTransmit.Location = new System.Drawing.Point(0, 206);
+            this.txtTransmit.Multiline = true;
+            this.txtTransmit.Name = "txtTransmit";
+            this.txtTransmit.Size = new System.Drawing.Size(380, 138);
+            this.txtTransmit.TabIndex = 4;
+            this.txtTransmit.TextChanged += new System.EventHandler(this.txtTransmit_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 347);
+            this.Controls.Add(this.txtTransmit);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtTransmit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSerialPort
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort Port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBox();
+        }
+
+        private void InitializeComboBox()
+        {
+            cmbPort.DataSource = SerialPort.GetPortNames();
+            cmbPort.SelectedIndex = 0;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Port = new SerialPort(portName: cmbPort.Text, baudRate: 9600, parity: Parity.None);
+                Port.Open();
+                if (Port.IsOpen) txtReceive.Text = $"Connected to port {Port.PortName}";
+                while (Port.IsOpen)
+                {
+                    txtReceive.Text += Port.ReadExisting();
+                    Application.DoEvents();
+                }
+            }
+            catch (Exception ee) { MessageBox.Show(ee.Message); }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Port.Close();
+            txtReceive.Text += $"\r\nPort {Port.PortName} closed.";
+        }
+
+        private void txtTransmit_TextChanged(object sender, EventArgs e)
+        {
+            if (Port.IsOpen) Port.Write(txtTransmit.Text.Last().ToString());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Port?.Close();
+        }
+    }
+}
